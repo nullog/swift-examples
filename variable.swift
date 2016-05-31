508e1230d33fdd5f94870e68d99cece15021c11a
huaixiaoz <hello@itmp.top>
2016-05-31 23:28:44 +0800

    variable.swift

---
+++ variable.swift
@@ -0,0 +1,13 @@
+var myVariable = 42
+myVariable = 50
+let myCOnstant = 42
+
+let imlicitInteger = 70
+let imlicitDouble = 70.0
+let explicitDouble: Double = 70
+
+let label = "Hello"
+let name = 007
+print(name)
+print (label + String(name))
+print (label + "\(name)")
