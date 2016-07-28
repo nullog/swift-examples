ae6731916b88870503585ebfcd21400025e19122
hello <hello@itmp.top>
2016-07-28 04:13:50 +0000

    test.swift

---
+++ example/test.swift
@@ -0,0 +1,35 @@
+func returnFifteen() -> Int {
+var y = 10
+func add() {
+y += 5
+}
+add()
+return y
+}
+
+func makeIncrementer() -> ((Int) -> Int) {
+func addOne(number: Int) -> Int {
+return 1 + number
+}
+return addOne
+}
+var increment = makeIncrementer()
+
+func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
+for item in list {
+if condition(item) {
+return true
+}
+}
+return false
+}
+func lessThanTen(number: Int) -> Bool {
+return number < 10
+}
+var numbers = [20, 19, 7, 12]
+
+print(returnFifteen())
+print(increment(7))
+print(hasAnyMatches(list: numbers, condition: lessThanTen))
+
+
